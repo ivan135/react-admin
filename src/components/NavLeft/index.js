0a0983c1814ab0a52777780c5849cca4e11f777e
ivan135 <1576475704@qq.com>
2018-12-09 21:57:53 +0800

    添加common.less

---
+++ src/components/NavLeft/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Menu, Icon } from 'antd';
+
+
+export default class NavLeft extends React.Component{
+    render(){
+        return (
+            <div>This is Header</div>
+        )
+    }
+}
