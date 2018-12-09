9ac746240871a10f344126018e0c142780aebe86
ivan135 <1576475704@qq.com>
2018-12-09 18:09:40 +0800

    初始化项目和完成项目结构

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Admin from './admin'
+import registerServiceWorker from './registerServiceWorker';
+// import Router from './router'
+// import { Provider } from 'react-redux'
+// import configureStore from './redux/store/configureStore';
+
+
+
+// // Redux Store对象，管理所有的Redux状态
+// const store = configureStore();
+// ReactDOM.render(
+//     <Provider store={store}>
+//         <Router />
+//     </Provider>,
+//     document.getElementById('root')
+// );
+// ReactDOM.render(<Router />, document.getElementById('root'));
+ReactDOM.render(<Admin />, document.getElementById('root'));
+registerServiceWorker();
